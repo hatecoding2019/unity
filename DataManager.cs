00876e94a443bd70a22ad373f77969d27131a742
codingLife2021 <58480139+hatecoding2019@users.noreply.github.com>
2022-01-10 15:19:24 +0100

    Add files via upload

---
+++ DataManager.cs
@@ -0,0 +1,162 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataManager : MonoBehaviour
+{
+    public static string fileName = "";
+    public static int totalRounds = 60;
+    public static int totalDeskRotateCount = 30;
+    //public static int totalnotDeskRotateCount = 30;
+    //public static int doRotate_changeItemPosCount = 30;
+    public static int changeItemPosCount = 15;
+    public static float doRotate_allTime = 0;
+    public static float notRotate_allTime = 0;
+    public static int groupIndex = -1; //1£ºA£¬2£ºB
+    public static string groupName = "";
+    public static int notRotate = 0;
+    public static int notRotate_hits = 0;
+    public static int notRotate_misses = 0;
+    public static int notRotate_falseAlarms = 0;
+    public static int notRotate_rejections = 0;
+    public static float notRotate_reactionTime = 0;
+
+    public static int doRotate = 0;
+    public static int doRotate_hits = 0;
+    public static int doRotate_misses = 0;
+    public static int doRotate_falseAlarms = 0;
+    public static int doRotate_rejections = 0;
+    public static float doRotate_reactionTime = 0;
+
+    public static string data = string.Format(
+        "Experiment Condition {0}\n" +
+        "Table does not rotate - \n" +
+        "Hits:{1}\n" +
+        "Misses:{2}\n" +
+        "False Alarms:{3}\n" +
+        "Rejection:{4}\n" +
+        "Mean reaction time:{5}\n" +
+        "Table does rotate - \n" +
+        "Hits:{6}\n" +
+        "Misses:{7}\n" +
+        "False Alarms:{8}\n" +
+        "Rejection:{9}\n" +
+        "Mean reaction time:{10}\n",
+        groupName,
+        notRotate_hits,
+        notRotate_misses,
+        notRotate_falseAlarms,
+        notRotate_rejections,
+        notRotate_reactionTime,
+        doRotate_hits,
+        doRotate_misses,
+        doRotate_falseAlarms,
+        doRotate_rejections,
+        doRotate_reactionTime
+        );
+
+
+
+    private void Start()
+    {
+        ResetData();
+        //FileManager.Instance.SaveData("nameB", data);
+    }
+
+
+    //------------------------unlit------------
+
+    public static void SetGroup(int index)
+    {
+        groupIndex = index;
+        if (groupIndex == 0)
+        {
+            groupName = "A";
+        }
+        else
+        {
+            groupName = "B";
+        }
+    }
+
+    public static void SaveData()
+    {
+        print("notRotate_allTime" + notRotate_allTime);
+        print("doRotate_allTime" + notRotate_allTime);
+        notRotate_reactionTime = notRotate_allTime / totalDeskRotateCount;
+        doRotate_reactionTime = doRotate_allTime/(totalRounds- totalDeskRotateCount);
+        fileName = fileName + "_" + groupName;
+           data = string.Format(
+          "Experiment Condition {0}\n" +
+          "Table does not rotate - \n" +
+          "Hits:{1}\n" +
+          "Misses:{2}\n" +
+          "False Alarms:{3}\n" +
+          "Rejection:{4}\n" +
+          "Mean reaction time:{5}\n" +
+          "Table does rotate - \n" +
+          "Hits:{6}\n" +
+          "Misses:{7}\n" +
+          "False Alarms:{8}\n" +
+          "Rejection:{9}\n" +
+          "Mean reaction time:{10}\n",
+          groupName,
+          notRotate_hits,
+          notRotate_misses,
+          notRotate_falseAlarms,
+          notRotate_rejections,
+          notRotate_reactionTime,
+          doRotate_hits,
+          doRotate_misses,
+          doRotate_falseAlarms,
+          doRotate_rejections,
+          doRotate_reactionTime
+          );
+
+        FileManager.Instance.SaveData(fileName, data);
+    }
+
+    public static void ResetData()
+    {
+        notRotate = 0;
+        notRotate_hits = 0;
+        notRotate_misses = 0;
+        notRotate_falseAlarms = 0;
+        notRotate_rejections = 0;
+        notRotate_reactionTime = 0;
+
+        doRotate = 0;
+        doRotate_hits = 0;
+        doRotate_misses = 0;
+        doRotate_falseAlarms = 0;
+        doRotate_rejections = 0;
+        doRotate_reactionTime = 0;
+
+        data = string.Format(
+       "Experiment Condition {0}\n" +
+       "Table does not rotate - \n" +
+       "Hits:{1}\n" +
+       "Misses:{2}\n" +
+       "False Alarms:{3}\n" +
+       "Rejection:{4}\n" +
+       "Mean reaction time:{5}\n" +
+       "Table does rotate - \n" +
+       "Hits:{6}\n" +
+       "Misses:{7}\n" +
+       "False Alarms:{8}\n" +
+       "Rejection:{9}\n" +
+       "Mean reaction time:{10}\n",
+       groupName,
+       notRotate_hits,
+       notRotate_misses,
+       notRotate_falseAlarms,
+       notRotate_rejections,
+       notRotate_reactionTime,
+       doRotate_hits,
+       doRotate_misses,
+       doRotate_falseAlarms,
+       doRotate_rejections,
+       doRotate_reactionTime
+       );
+    }
+}
---
+++ FileManager.cs
@@ -0,0 +1,101 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+using System.Text;
+using System;
+
+public class FileManager : MonoBehaviour
+{
+    public static FileManager Instance;
+
+
+    //string tableA = "Experiment Condition A\n";
+    //string tableB = "Experiment Condition B\n";
+    //string normalTablenot = "Table does not rotate - \n";
+    //string normalTable = "Table does rotate - \n";
+    //string Hits = "Hits:\n";
+    //string Misses = "Misses:\n";
+    //string FalseAlarms = "False Alarms:\n";
+    //string Rejection = "Rejection:\n";
+    //string reactionTime = "Mean reaction time:\n";
+
+    //List<byte> strLists = new List<byte>();
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void AddByte(List<byte> testlis,string strr)
+    {
+        byte[] temp = Encoding.UTF8.GetBytes(strr);
+        for (int i = 0; i < temp.Length; i++)
+        {
+            testlis.Add(temp[i]);
+        }
+    }
+
+    public void SaveData(string name, string info)
+    {
+        byte[] bytes = Encoding.ASCII.GetBytes(info.ToCharArray());
+
+        string normalPath = "";
+        normalPath = Application.dataPath + "/Code/data/"+ name+".txt";
+        //if (isA)
+        //{
+        //    normalPath = Application.dataPath + "/Code/data/filename_A.txt";
+        //    AddByte(strLists, tableA);
+        //}
+        //else
+        //{
+        //    normalPath = Application.dataPath + "/Code/data/filename_B.txt";
+        //    AddByte(strLists, tableB);
+        //}
+ 
+        //AddByte(strLists, normalTablenot);
+        //AddByte(strLists, Hits);
+        //AddByte(strLists, Misses);
+        //AddByte(strLists, FalseAlarms);
+        //AddByte(strLists, Rejection);
+        //AddByte(strLists, reactionTime);
+        //AddByte(strLists, normalTable);
+        //AddByte(strLists, Hits);
+        //AddByte(strLists, Misses);
+        //AddByte(strLists, FalseAlarms);
+        //AddByte(strLists, Rejection);
+        //AddByte(strLists, reactionTime);
+
+        //byte[] bts = strLists.ToArray();
+        bool isExist = File.Exists(normalPath);
+        //print(isExist);
+        if (isExist)
+        {
+            FileStream file = new FileStream(normalPath, FileMode.Open);
+            file.Write(bytes, 0, bytes.Length);
+            if (file != null)
+            {
+                file.Flush();
+                file.Close();
+                file.Dispose();
+            }
+        }
+        else
+        {
+            // 文件流创建一个文本文件
+            FileStream file = new FileStream(normalPath, FileMode.Create);
+            print(bytes.Length);
+            // 文件写入数据流
+            file.Write(bytes, 0, bytes.Length);
+            if (file != null)
+            {
+                //清空缓存
+                file.Flush();
+                // 关闭流
+                file.Close();
+                //销毁资源
+                file.Dispose();
+            }
+        }
+    }
+}
